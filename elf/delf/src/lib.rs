3cc5a13be8d184f5e35e535c4198862a3aca1d9d
Mathieu Lapointe <matlapo.dev@gmail.com>
2021-03-22 20:01:32 -0400

    elf

---
+++ elf/delf/src/lib.rs
@@ -0,0 +1,38 @@
+mod parse;
+
+#[derive(Debug)]
+pub struct File {
+    // we'll add fields as we go
+}
+
+impl File {
+    const MAGIC: &'static [u8] = &[0x7f, 0x45, 0x4c, 0x46];
+
+    pub fn parse(i: parse::Input) -> parse::Result<Self> {
+        use nom::{
+            bytes::complete::{tag, take},
+            error::context,
+            sequence::tuple,
+        };
+        let (i, _) = tuple((
+            // -------
+            context("Magic", tag(Self::MAGIC)),
+            context("Class", tag(&[0x2])),
+            context("Endianness", tag(&[0x1])),
+            context("Version", tag(&[0x1])),
+            context("OS ABI", nom::branch::alt((tag(&[0x0]), tag(&[0x3])))),
+            // -------
+            context("Padding", take(8_usize)),
+        ))(i)?;
+        Ok((i, Self {}))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
